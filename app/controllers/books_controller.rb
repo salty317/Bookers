733f72619a535e938c5548f2684df255ffe3ebee
Shion Tanaka <b8219@oic.jp>
2020-06-01 00:29:58 +0900

    [Update] バリデーションエラー時は再表示するように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,17 +13,24 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created"
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated"
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      render action: :edit
+    end
   end
 
   def destroy

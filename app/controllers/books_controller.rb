518966272a15ff83558808b794282220b80a95cc
Shion Tanaka <b8219@oic.jp>
2020-05-31 23:46:12 +0900

    [Update] 投稿・更新成功後、サクセスメッセージを表示するに変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,12 +15,14 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created"
     redirect_to books_path
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated"
     redirect_to book_path(book)
   end
 

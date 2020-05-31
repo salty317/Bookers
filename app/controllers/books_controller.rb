e3b68bfb67570c86381326fa7ef059c2ce5e157a
Shion Tanaka <b8219@oic.jp>
2020-05-31 23:47:07 +0900

    [Fix] リダイレクト先修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.save
     flash[:notice] = "Book was successfully created"
-    redirect_to books_path
+    redirect_to book_path(book)
   end
 
   def update
